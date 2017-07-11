29d602a36df91ae287a77eab156d4353b46e1011
Marti Bayo Alemany <martibayoalemany@gmail.com>
2017-07-11 04:53:45 +0200

    Update stats_binary_search.sh

--- scripts/stats_binary_search.sh
+++ scripts/stats_binary_search.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env sh
 SCRIPT_PATH=$(realpath $(dirname $0))
-#$SCRIPT_PATH/perf_binary_gap.sh  | egrep -e '(user|sys|real)' | xargs -l3
 $SCRIPT_PATH/perf_binary_gap.sh  | egrep -e 'user' 
