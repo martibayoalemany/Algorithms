7aa3300f86e68e04a23762cb0a6a3df472f9248a
Marti Bayo Alemany <martibayoalemany@gmail.com>
2017-07-30 01:05:13 +0200

    Update BinaryGap.cs

--- src/main/cs/BinaryGap.cs
+++ src/main/cs/BinaryGap.cs
@@ -8,7 +8,7 @@ namespace cs
     public class BinaryGap
     {
 
-        public static void Main2(string[] args)
+        public static void Main(string[] args)
         {            
             
             int result = execute(654345);
